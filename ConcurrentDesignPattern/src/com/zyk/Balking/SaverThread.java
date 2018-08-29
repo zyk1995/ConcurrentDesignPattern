68bcee33cacef09bd6ba70cc18fd9c7faa9d49a6
zyk-um <1251134350@qq.com>
2018-08-29 17:13:35 +0800

    Balking模式

---
+++ ConcurrentDesignPattern/src/com/zyk/Balking/SaverThread.java
@@ -0,0 +1,27 @@
+package com.zyk.Balking;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+
+public class SaverThread extends  Thread {
+    private  final  Data data;
+
+    public SaverThread(String name, Data data) {
+        super(name);
+        this.data = data;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true){
+                data.save();
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

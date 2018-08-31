1e281ad3728c4ee43e0e8a8aa009c05756319749
zyk-um <1251134350@qq.com>
2018-08-31 09:29:04 +0800

    WorkerThread模式

---
+++ ConcurrentDesignPattern/src/com/zyk/WorkerThread/Request.java
@@ -0,0 +1,28 @@
+package com.zyk.WorkerThread;
+
+import java.util.Random;
+
+public class Request {
+    private final String name;  //委托者
+    private final int number;   //请求的编号
+    private static final Random random = new Random();
+
+    public Request(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public void execute() {
+        System.out.println(Thread.currentThread());
+        try {
+            Thread.sleep(random.nextInt(1000));
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[ Request from ] " + name + " No." + number + " ]";
+    }
+}
